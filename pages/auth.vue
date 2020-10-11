ed825d1191d8dfe82e8bf1d43284ec249d886e83
Denis Rudnei de Souza <denisrudneidesouza@gmail.com>
2020-10-11 13:00:51 -0300

    Added navigation drawer in auth page

---
+++ pages/auth.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row>
+    <v-navigation-drawer app>
+      <v-list>
+        <v-list-item to="/auth/login">
+          <v-list-item-icon>
+            <v-icon>
+              mdi-account-box
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            Login
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/auth/register">
+          <v-list-item-icon>
+            <v-icon>
+              mdi-book-information-variant
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            Register
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-col>
+      <v-card>
+        <v-card-text>
+          <nuxt-child />
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+</style>
